66232705335af1835e89e7ac7d14ca65bcf84af1
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-04-01 13:48:42 -0400

    added a class to display the difference between Stack (value type) and Heaps (reference type)

--- IntroCSharp/IntroCSharp/Program.cs
+++ IntroCSharp/IntroCSharp/Program.cs
@@ -1,8 +1,66 @@
-﻿Console.WriteLine("Hello, World!");
-String sentence = "This is a sentence written in C-Sharp.";
+﻿using IntroCSharp;
+
+Console.WriteLine("Hello, World!\n");
+string sentence = "This is a sentence written in C-Sharp.";
 int age = 35;
 char letter = 'A';
+Boolean isTrue = false;
+double valueDouble = 7 / 2;
+float valueFloat = 14.3f;
+var value = 13 % 5;
+
 
 Console.WriteLine(sentence);
 Console.WriteLine(age);
 Console.WriteLine(letter);
+Console.WriteLine(value);
+Console.WriteLine(valueFloat);
+//Output only the last 2 digits after the point.
+Console.WriteLine($"{valueDouble:F2}");
+Console.WriteLine(isTrue);
+
+int increment = 1;
+
+Console.WriteLine(increment=+1);
+Console.WriteLine(++increment);
+
+//TYPE CASTING
+//implicit casting
+int i = 123123;
+long bigNum = i;
+Console.WriteLine(bigNum);
+
+//explicit casting
+double x = 123.312;
+int a = (int)x;
+
+Console.WriteLine(a);
+
+// String to int
+string stringValue = "123214";
+int _ = 0;
+if(int.TryParse(stringValue, out _))
+{
+    Console.WriteLine("Success: "+_+"\n");
+}
+else
+{
+    Console.WriteLine("Failure.\n");
+}
+
+// Stack and Heap. Stack is for value type and a Heap is for reference types.
+int intValue = 4;
+byte byteValue = 12;
+bool boolValue = false;
+string valueString = "New Value";
+
+Console.WriteLine(intValue);
+Console.WriteLine(byteValue);
+Console.WriteLine(boolValue);
+Console.WriteLine(valueString);
+
+//import a class
+StackHeap stackHeap = new StackHeap();
+
+Console.WriteLine(stackHeap.addFive(15));
+
---
+++ IntroCSharp/IntroCSharp/StackHeap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroCSharp
+{
+    public class StackHeap
+    {
+        public StackHeap() { }
+
+        public int addFive(int val)
+        {
+
+            int result;
+            result = val + 5;
+
+            return result;
+        }
+    }
+}
