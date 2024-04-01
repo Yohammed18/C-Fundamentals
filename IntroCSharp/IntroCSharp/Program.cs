7dea70834905fb06bdd540e5b01adb981fd3c2c2
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-04-01 15:22:39 -0400

    more info on C3 (StringBuilder, LINQ, literals vs verbatim, Interpolation + Composite Formating, etc...

--- IntroCSharp/IntroCSharp/Program.cs
+++ IntroCSharp/IntroCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using IntroCSharp;
+using System.Text;
 
 Console.WriteLine("Hello, World!\n");
 string sentence = "This is a sentence written in C-Sharp.";
@@ -62,5 +63,60 @@ Console.WriteLine(valueString);
 //import a class
 StackHeap stackHeap = new StackHeap();
 
-Console.WriteLine(stackHeap.addFive(15));
+Console.WriteLine(stackHeap.addFive(15
+    ));
+
+
+//Arrays - a fix collection of variable once created.
+int[] num = { -2,6,10,192,-3,2,1,0 };
+string[] string_array = new string[num.Length];
+
+string_array[0] = "james";
+string_array[1] = "michael";
+string_array[2] = "kobe";
+string_array[3] = "Smith";
+
+foreach (var item in string_array)
+{
+    Console.WriteLine(item); 
+}
+num[4] = 12;
+Console.WriteLine("Array Length: "+num.Length);
+Console.WriteLine("["+string.Join(", ", num)+"]");
+
+
+// Learn Linq framework -  a feature in C# that provides a uniform way to query data from different data sources. LINQ allows you to query collections, databases, XML documents, and other data sources using a syntax similar to SQL.
+var numAverage = num.Average();
+
+Console.WriteLine(numAverage);
+
+// sort array
+Array.Sort(num);
+Console.WriteLine("[" + string.Join(", ", num) + "]");
+
+String string_name = "mohammed";
+
+Console.WriteLine(string_name.ToUpper());
+
+//Value types CANNOT BE NULL
+
+//literal vs Verbatim
+string fileLiteral = "c:\\ProgramFiles";
+
+string fileVerbatim = @"c:\ProgramFiles";
+
+//String are immutabile (Read Only)
+
+//String Interpolation + Composite Formating
+string firstName = "Luca";
+string lastName = "Smith";
+//String Interpolation
+Console.WriteLine($"My name is {firstName} {lastName}");
+//Composite Formating (unlike python you need to be explicit and label the bracket with the index
+Console.WriteLine("Name: {0}\nSurname: {1}\n", firstName, lastName);
+
+//StringBuilder (mutable unlike String)
+var sb = new StringBuilder("Hello World");
+
+Console.WriteLine(sb.ToString());
 
