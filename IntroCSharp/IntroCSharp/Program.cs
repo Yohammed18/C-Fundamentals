07335be37d99739a20359ce102e5b708c37d1f85
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-04-01 11:04:01 -0400

    Added the first c sharp file.

---
+++ IntroCSharp/IntroCSharp/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+String sentence = "This is a sentence written in C-Sharp.";
+int age = 35;
+char letter = 'A';
+
+Console.WriteLine(sentence);
+Console.WriteLine(age);
+Console.WriteLine(letter);
