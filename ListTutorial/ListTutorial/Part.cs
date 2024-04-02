9a46fddaf735e0e431d3289f80faedd6045c7566
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-04-02 08:24:52 -0400

    C# progress update.

---
+++ ListTutorial/ListTutorial/Part.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTutorial
+{
+    public class Part
+    {
+        public string PartName { get; set; }
+
+        public int PartId { get; set; }
+
+    }
+}
---
+++ ListTutorial/ListTutorial/Program.cs
@@ -0,0 +1,13 @@
+﻿// Lists in C#
+using ListTutorial;
+
+List<Part> lp = new List<Part>();
+
+lp.Add(new Part{PartName = "Headlight", PartId = 1});
+lp.Add(new Part { PartName = "Bumper", PartId = 2 });
+
+
+foreach (Part p in lp)
+{
+    Console.WriteLine("Part: {0}\nId: {1}",p.PartName, p.PartId);
+}
