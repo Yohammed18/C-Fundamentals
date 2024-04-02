0d4ff16280b8a6ca8c2c50e181c82435397f7a4a
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-04-02 14:09:14 -0400

    example of OOP to include classes, interface, polymorphism, abstract, etc..

---
+++ OOP/OOP/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    //Classes 
+    public class Movie
+    {
+        //noun
+        private string title { get; set; }
+        private string director { get; set; }
+        private string rating { get; set; }
+
+        //constructor
+        public Movie() { }
+        public Movie(string title, string director, string rating)
+        {
+            this.title = title;
+            this.director = director;
+            this.rating = rating;
+        }
+
+        //action
+
+    }
+}
