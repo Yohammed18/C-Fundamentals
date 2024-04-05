b733f610c86ee0e65417189011aaf7661bcee404
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-04-05 09:59:42 -0400

    Create simple HelloWorld app with C#. Inclused interaction with the application asking the user's name and outputing it on the console.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,5 @@
+﻿//Simple C# application
+
+Console.WriteLine("Hello, World! Welcome to C#. What is your name?");
+var name = Console.ReadLine();
+System.Console.WriteLine($"Hey {name}. Enjoy the C# Fundementals tutorial.");
