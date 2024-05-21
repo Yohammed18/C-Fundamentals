548fe702e4f95c04812199d7f047da91d835ee8f
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-05-21 16:54:07 -0400

    Created Controller, add services, utilize attributes ApiController and Route to configure controller and created function to handle CRUD requests.

---
+++ WebApiDemo/WebApiDemo/Controller/ShirtsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controller
+{
+    [ApiController]
+    public class ShirtsController: ControllerBase
+    {
+        [HttpGet]
+        [Route("/shirts")]
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
+
+        [HttpGet]
+        [Route("/shirt/{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"Reading shirt with ID: {id}";
+        }
+
+        [HttpPut]
+        [Route("/update/{id}")]
+        public string UpdateShirtById(int id)
+        {
+            return $"Updating shirt with ID: {id}";
+        }
+
+        [HttpDelete]
+        [Route("/delete/{id}")]
+        public void DeleteShirtById(int id)
+        {
+            Console.WriteLine($"Deleting shirt with ID: {id}");
+        }
+        [HttpPost]
+        [Route("/shirt")]
+        public string CreateShirt()
+        {
+            return "Creating a shirt.";
+        }
+    }
+}
